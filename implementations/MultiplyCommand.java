310590544d90b9e724d07758dcdb95a6330c5f4a
vs8-singh <vsb.singh80@gmail.com>
2024-02-14 09:11:02 +0530

    implemented command interfaces

---
+++ implementations/MultiplyCommand.java
@@ -0,0 +1,10 @@
+package implementations;
+
+import interfaces.Command;
+
+public class MultiplyCommand implements Command {
+    @Override
+    public void execute(int num1, int num2) {
+
+    }
+}
---
+++ interfaces/Command.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public interface Command {
+    void execute(int num1, int num2);
+}
